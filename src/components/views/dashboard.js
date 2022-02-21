4c54359f5bdd5c0ca52ed9e4eca43ab8200bde70
aditya <adityapfm99@gmail.com>
2022-02-21 16:05:25 +0700

    update dashboard

--- src/components/views/dashboard.js
+++ src/components/views/dashboard.js
@@ -1,49 +1,81 @@
-import React from "react"
-import ChartReportCpoPerpulau from "../../utilities/chart/chartReportCpoPerpulau"
-import ChartReportPerPulau from "../../utilities/chart/chartReportPerPulau"
-import ChartExportCPO from "../../utilities/chart/exportCPO"
-import ChartProductTbs from "../../utilities/chart/productTbs"
-import TableDashboard from "../../utilities/tableDashboard"
-import TableReportPerGroup from "../../utilities/tableReportPerGroup"
+import React from "react";
+import { Input, Button, FormGroup, Label } from "reactstrap";
+import ChartReportCpoPerpulau from "../../utilities/chart/chartReportCpoPerpulau";
+import ChartReportPerPulau from "../../utilities/chart/chartReportPerPulau";
+import ChartExportCPO from "../../utilities/chart/exportCPO";
+import ChartProductTbs from "../../utilities/chart/productTbs";
+import TableDashboard from "../../utilities/tableDashboard";
+import TableReportPerGroup from "../../utilities/tableReportPerGroup";
 
 const Dashboard = () => {
-    return (
-        <>
-            <div className="container">
-                <div className="row">
-                    <div className="col-12">
-                        <h1 style={{ marginTop: "2rem" }}>Dashboard</h1>
-                    </div>
-                </div>
-                <div className="row" style={{ marginTop: "3em" }}>
-                    <div className="col-6">
-                        <ChartProductTbs/>
-                    </div>
-                    <div className="col-6">
-                        <ChartExportCPO/>
-                    </div>
-                </div>
-                <div className="row" style={{ marginTop: "3em" }}>
-                    <div className="col-12">
-                        <TableDashboard/>
-                    </div>
-                </div>
-                <div className="row" style={{ marginTop: "3em", marginBottom: "3em",  display: 'flex', justifyContent:'space-between', alignContent:'center' }}>
-                    <div className="col-6">
-                        <ChartReportPerPulau/>
-                    </div>
-                    <div className="col-4">
-                        <ChartReportCpoPerpulau />
-                    </div>
-                </div>
-                {/* <div className="row" style={{ marginTop: "3em" }}>
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <h1 style={{ marginTop: "2rem" }}>Dashboard</h1>
+            <hr></hr>
+          </div>
+        </div>
+        <div className="col-0">
+          <FormGroup>
+            <div className="row">
+              
+              <div className="col-1">
+                <Label for="tahun">Tahun</Label>
+              </div>
+              <div className="col-2">
+                <Input name="tahun" type="select">
+                  <option>2020</option>
+                  <option>2021</option>
+                  <option>2022</option>
+                  <option>2023</option>
+                  <option>2024</option>
+                  <option>2025</option>
+                </Input>
+              </div>
+              
+            </div>
+          </FormGroup>
+        </div>
+        <div className="row" style={{ marginTop: "3em" }}>
+          <div className="col-6">
+            <ChartProductTbs />
+          </div>
+          <div className="col-6">
+            <ChartExportCPO />
+          </div>
+        </div>
+        <div className="row" style={{ marginTop: "3em" }}>
+          <div className="col-12">
+            <TableDashboard />
+          </div>
+        </div>
+        <div
+          className="row"
+          style={{
+            marginTop: "3em",
+            marginBottom: "3em",
+            display: "flex",
+            justifyContent: "space-between",
+            alignContent: "center",
+          }}
+        >
+          <div className="col-6">
+            <ChartReportPerPulau />
+          </div>
+          <div className="col-4">
+            <ChartReportCpoPerpulau />
+          </div>
+        </div>
+        {/* <div className="row" style={{ marginTop: "3em" }}>
                     <div className="col-12">
                         <TableReportPerGroup />
                     </div>
                 </div> */}
-            </div>
-        </>
-    )
-}
+      </div>
+    </>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
